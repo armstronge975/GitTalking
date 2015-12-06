1d7fdd6369ca821d80ade9345f1e79ed23aad4c0
Emily Armstrong <armstronge975@strose.edu>
2015-12-06 14:27:53 -0500

    Adding timeline implementation files to retrieve timeline from db

---
+++ GitTalking/src/main/java/com/foobar/gittalking/TimelineDAO.java
@@ -0,0 +1,8 @@
+package com.foobar.gittalking;
+
+import java.sql.*;
+
+public interface TimelineDAO {
+	public Timeline findStandardTimeline(String userID) throws SQLException;
+	public Timeline findAdminTimeline(String userID) throws SQLException;
+}
